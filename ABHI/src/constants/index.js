c3899476db9e8af58c3b4f9f162553ba6f6fe0aa
Abhi-lash27 <babhilash27@gmail.com>
2024-06-05 20:02:39 +0530

    first commit

---
+++ ABHI/src/constants/index.js
@@ -0,0 +1,94 @@
+import project1 from "../assets/projects/BAKEIT.png";
+import project2 from "../assets/projects/GUIDE.png";
+import project3 from "../assets/projects/PORTFOLIO.png";
+import project4 from "../assets/projects/MEDPREP.png";
+
+export const HERO_CONTENT = `I am a dedicated B.Tech Information Technology student with a keen interest in full stack web development, graphic design, and various software technologies. With a strong foundation in both front-end and back-end development, I aim to create innovative and user-friendly web applications, graphic designs. I am passionate about learning and applying new skills to solve real-world problems and contribute to impactful projects.`;
+
+export const ABOUT_TEXT = `I am a B.Tech Information Technology student with a strong passion for full stack web development, graphic design, and software technologies. Throughout my academic journey, I have developed skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. Additionally, I have explored graphic design tools and techniques, which allow me to create visually appealing and functional user interfaces.\n\nI have also gained practical experience through internships in full stack development, where I worked on real-world projects, honed my coding skills, and collaborated with professional teams. My goal is to leverage my diverse skill set to work on innovative projects that enhance user experiences and drive technological advancement.\n\nIn my free time, I enjoy experimenting with new technologies, participating in hackathons, and collaborating on open-source projects.`;
+
+
+export const EXPERIENCES = [
+  {
+    "year": "19-12-2023 to Present",
+    "role": "Software Engineer",
+    "company": "VersalGo PVT.LTD",
+    "description": "As a Software Engineer at VersalGo PVT.LTD, I specialize in frontend development, contributing to the creation and enhancement of user interfaces for web applications. Leveraging my expertise in JavaScript, React.js, and Next.js, I design and implement intuitive and visually appealing frontend solutions that optimize user experience. I collaborate closely with design teams and stakeholders to translate user requirements into functional frontend components. Additionally, I continuously explore emerging frontend technologies and best practices to deliver cutting-edge solutions.",
+    "technologies": ["Javascript", "React.js", "Next.js", "Postgresql"]
+  },
+  {
+    year: "22-01-2024 to 30-01-2024",
+    role: "Frontend Developer",
+    company: "HackWit Technologies",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend development to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["MongoDB", "Express", "React", "Node.js"],
+  },
+  {
+    year: "08-07-2023 to 31-08-2023",
+    role: "Full Stack Developer",
+    company: "HackWit Technologies",
+    description: `As a Full Stack Developer at HackWit Technologies, I played a key role in the development and maintenance of web applications using JavaScript, React.js, and Node.js. I was responsible for designing and implementing RESTful APIs for efficient data communication between the client-side and server-side components.`,
+    technologies: ["MongoDB", "Express", "React", "Node.js"],
+  },
+  {
+    year: "05-07-2023 to 31-10-2023",
+    role: "Graphic Designer",
+    company: "INFYSKILL Edutech Pvt.Ltd",
+    description: `As a Graphic Designer at INFYSKILL Edutech Pvt.Ltd, I was responsible for creating visually appealing designs and graphics for various formats. Utilizing tools like Adobe Illustrator, Canva, and Figma, I collaborated with the development team to integrate these designs seamlessly into the user interface.`,
+    technologies: ["Abobe Illustrator", "Canva", "Figma"],
+  },
+];
+
+export const CERTIFICATIONS = [
+  {
+    year:"August-2023",
+    title:"Python For Data Science",
+    provider:"NPTEL",
+    "description":"Certifies as Elite",
+  },
+  {
+    "year":"April-2023",
+    "title":"Programming in Java",
+    "provider":"NPTEL",
+    "description":"Certified as Elite",
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "Guide Selection Portal",
+    image: project2,
+    description:
+      "A live project developed for a college department, featuring user authentication. The portal facilitates the selection and management of project guides, allowing students and faculty to efficiently interact and track progress.",
+    technologies: ["HTML", "CSS", "React", "Node.js"]
+  },
+  {
+    title: "MedPrep",
+    image: project4,
+    description:
+      "A Hospital Management System (HMS) designed for efficient hospital administration, featuring authentication for different user roles including doctors, nurses, and patients. The system provides an advancement of multi-language system for static data.",
+    technologies: ["HTML", "CSS", "React", "Postgresql"],
+  },
+  {
+  title: "BAKE-IT (E-Commerce Website)",
+  image: project1,
+  description:
+    "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+  technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "React", "Tailwind"],
+  },
+
+];
+
+export const CONTACT = {
+  address: "17B, Varalakshmi Street, Rajeshwari Nagar, Selaiyur, Chennai - 600073 ",
+  phoneNo: "+91 73392 44717 ",
+  email: "babhilash27@gmail.com",
+};
